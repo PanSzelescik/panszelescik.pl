db4f5d45636a422f38969b2fc6a572907e319fde
PanSzelescik <panszelescik@gmail.com>
2025-07-13 01:20:12 +0200

    Tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import "./globals.css";
 import type { Metadata, Viewport } from "next";
 import { Nunito } from "next/font/google";
-import "./globals.css";
 import type { ReactNode } from "react";
 import Analytics from "@/app/lib/analytics/Analytics";
 
@@ -20,6 +20,44 @@ export const viewport: Viewport = {
 export const metadata: Metadata = {
   title: "PanSzelescik",
   description: "Developer from Poland",
+  authors: [
+    {
+      name: "PanSzelescik",
+    },
+  ],
+  openGraph: {
+    title: "PanSzelescik",
+    description: "Developer from Poland",
+    url: process.env.NEXT_PUBLIC_BASE_URL,
+    siteName: "PanSzelescik.pl",
+    locale: "pl_PL",
+    type: "website",
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/icon.png`,
+        width: 1200,
+        height: 1200,
+        alt: "PanSzelescik",
+      },
+    ],
+  },
+  twitter: {
+    site: "@PanSzelescik",
+    siteId: "911164970339110912",
+    creator: "@PanSzelescik",
+    creatorId: "911164970339110912",
+    card: "summary_large_image",
+    title: "PanSzelescik",
+    description: "Developer from Poland",
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/icon.png`,
+        width: 1200,
+        height: 1200,
+        alt: "PanSzelescik",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
