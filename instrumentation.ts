7d4abed411ce16def447056da29637bc85d6675c
PanSzelescik <panszelescik@gmail.com>
2025-07-14 00:14:09 +0200

    Idk

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,10 +1,11 @@
+import { registerOTel } from "@vercel/otel";
+
 export async function register() {
   if (
     process.env.NEXT_RUNTIME === "nodejs" &&
     process.env.OTEL_EXPORTER_OTLP_ENDPOINT &&
     process.env.OTEL_EXPORTER_OTLP_PROTOCOL
   ) {
-    // @ts-expect-error TS5097
-    await import("./instrumentation.node.ts");
+    registerOTel({ serviceName: "panszelescik" });
   }
 }
