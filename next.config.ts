d631d757105741d062bd2f2238ead9548ed91ae3
PanSzelescik <panszelescik@gmail.com>
2025-07-31 19:17:24 +0200

    ???

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,7 @@ const nextConfig: NextConfig = {
     serverActions: {
       allowedOrigins: ["panszelescik.pl"],
     },
+    esmExternals: "loose",
   },
   allowedDevOrigins: ["panszelescik.pl", "192.168.1.100"],
   headers: async () => [
