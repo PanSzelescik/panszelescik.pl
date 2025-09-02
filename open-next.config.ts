fce0b2abe6819bc9f1cba7e5200305d10f53be3e
PanSzelescik <panszelescik@gmail.com>
2025-09-02 10:34:39 +0200

    Fix

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,14 +1,7 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-import d1NextTagCache from "@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache";
-import memoryQueue from "@opennextjs/cloudflare/overrides/queue/memory-queue";
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
-import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";
+import staticAssetsIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/static-assets-incremental-cache";
 
 export default defineCloudflareConfig({
-  incrementalCache: withRegionalCache(r2IncrementalCache, {
-    mode: "long-lived",
-    bypassTagCacheOnCacheHit: true,
-  }),
-  tagCache: d1NextTagCache,
-  queue: memoryQueue,
+  incrementalCache: staticAssetsIncrementalCache,
+  enableCacheInterception: true,
 });
