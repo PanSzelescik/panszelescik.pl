3bc9147abb17baba25f66ec27f92624d09c995d6
PanSzelescik <panszelescik@gmail.com>
2025-09-17 10:55:19 +0200

    KV

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,20 +1,22 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 import d1NextTagCache from "@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache";
 import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
-import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";
 import queueCache from "@opennextjs/cloudflare/overrides/queue/queue-cache";
 import {
   softTagFilter,
   withFilter,
 } from "@opennextjs/cloudflare/overrides/tag-cache/tag-cache-filter";
+import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
+/*import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";*/
 
 export default defineCloudflareConfig({
-  incrementalCache: withRegionalCache(r2IncrementalCache, {
+  /*incrementalCache: withRegionalCache(r2IncrementalCache, {
     mode: "long-lived",
     shouldLazilyUpdateOnCacheHit: true,
     bypassTagCacheOnCacheHit: true,
-  }),
+  }),*/
+  incrementalCache: kvIncrementalCache,
   queue: queueCache(doQueue, {
     regionalCacheTtlSec: 5,
     waitForQueueAck: true,
