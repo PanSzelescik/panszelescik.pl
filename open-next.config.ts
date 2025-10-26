4fb9e1326eec1eb7e1f7c256d9a18c54d54243ab
PanSzelescik <panszelescik@gmail.com>
2025-10-26 15:54:07 +0100

    Change cache

--- open-next.config.ts
+++ open-next.config.ts
@@ -6,17 +6,15 @@ import {
   softTagFilter,
   withFilter,
 } from "@opennextjs/cloudflare/overrides/tag-cache/tag-cache-filter";
-import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
-/*import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
-import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";*/
+import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";
 
 export default defineCloudflareConfig({
-  /*incrementalCache: withRegionalCache(r2IncrementalCache, {
+  incrementalCache: withRegionalCache(r2IncrementalCache, {
     mode: "long-lived",
     shouldLazilyUpdateOnCacheHit: true,
     bypassTagCacheOnCacheHit: true,
-  }),*/
-  incrementalCache: kvIncrementalCache,
+  }),
   queue: queueCache(doQueue, {
     regionalCacheTtlSec: 5,
     waitForQueueAck: true,
@@ -26,5 +24,4 @@ export default defineCloudflareConfig({
     filterFn: softTagFilter,
   }),
   enableCacheInterception: true,
-  //cachePurge: purgeCache({ type: "direct" }),
 });
