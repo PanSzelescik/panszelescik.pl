188ffdda090095318d7c239b99cf726969f50e15
PanSzelescik <panszelescik@gmail.com>
2026-02-13 22:49:31 +0100

    Updates

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,23 +1,26 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";
 import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
 import queueCache from "@opennextjs/cloudflare/overrides/queue/queue-cache";
-import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
+import d1NextTagCache from "@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache";
+import {
+  withFilter,
+  softTagFilter,
+} from "@opennextjs/cloudflare/overrides/tag-cache/tag-cache-filter";
 
 export default defineCloudflareConfig({
-  /*incrementalCache: withRegionalCache(r2IncrementalCache, {
+  incrementalCache: withRegionalCache(r2IncrementalCache, {
     mode: "long-lived",
     shouldLazilyUpdateOnCacheHit: true,
-    bypassTagCacheOnCacheHit: true,
-  }),*/
-  incrementalCache: kvIncrementalCache,
+  }),
   queue: queueCache(doQueue, {
     regionalCacheTtlSec: 60,
     waitForQueueAck: false,
   }),
-  /*tagCache: withFilter({
+  tagCache: withFilter({
     tagCache: d1NextTagCache,
     filterFn: softTagFilter,
-  }),*/
+  }),
   enableCacheInterception: true,
-  //cachePurge: purgeCache({ type: "direct" }),
 });
