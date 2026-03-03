1d98f8683d27c8a4f397025ea6eb0ff9a7263f95
PanSzelescik <panszelescik@gmail.com>
2026-03-03 20:12:26 +0100

    Try KV

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,5 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
-import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";
+import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
 import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
 import queueCache from "@opennextjs/cloudflare/overrides/queue/queue-cache";
 import d1NextTagCache from "@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache";
@@ -10,10 +9,7 @@ import {
 } from "@opennextjs/cloudflare/overrides/tag-cache/tag-cache-filter";
 
 export default defineCloudflareConfig({
-  incrementalCache: withRegionalCache(r2IncrementalCache, {
-    mode: "long-lived",
-    shouldLazilyUpdateOnCacheHit: true,
-  }),
+  incrementalCache: kvIncrementalCache,
   queue: queueCache(doQueue, {
     regionalCacheTtlSec: 60,
     waitForQueueAck: false,
