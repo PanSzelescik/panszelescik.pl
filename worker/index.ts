a94e2e0f7348a8ff27e7b90688caad6ba4f1cb56
PanSzelescik <panszelescik@gmail.com>
2026-03-18 20:58:12 +0100

    Better

--- worker/index.ts
+++ worker/index.ts
@@ -37,7 +37,7 @@ interface ExecutionContext {
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     const url = new URL(request.url);
-    if (url.port === "2083") {
+    if (url.port !== "" && url.port !== "80" && url.port !== "443") {
       return new Response("Not Found", {
         status: 404,
         headers: { "Content-Type": "text/plain" },
